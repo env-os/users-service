6e4ed3a6618202b3b0dfe7fe3c64a34101596e7f
LucaGrilli <luca.grilli@studenti.unicam.it>
2019-05-30 16:09:17 +0200

    inserimento commenti a video per spiegazione funzioni

--- app/controllers/user.controller.ts
+++ app/controllers/user.controller.ts
@@ -12,6 +12,7 @@ export class UserController {
     
     @Post()
     async create(@Body() user: UserDto, @Res() res : Response) {
+        console.log("Received POST request for create a new user");
         const response = await this.userService.createUser(
             user.uid,
             user.username,
@@ -19,28 +20,35 @@ export class UserController {
             user.fullname,
             user.phone
         )
+        console.log(response);
         return res.send(response)
     }
 
     @Get()
     async getAll(): Promise<User[]> {
+        console.log("Received GET request to view all users");
         return await this.userService.getAllUsers();
     }
 
     @Get('/:username')
     async getOneByUsername(@Param('username') username: string): Promise<User> {
+        console.log("Received GET request to view a user");
         const response = await this.userService.getUserByUsername(username)
+        console.log(response);
         return response
     }
 
     @Get('/exist/:username')
     async userExist(@Param('username') username: string): Promise<boolean> {
+        console.log("Received GET request to know if the selected user exists");
         return await this.userService.userExist(username)
     }
 
     @Delete('/:username')
     async deleteUser(@Param('username') username: string, @Res() res : Response) {
+        console.log("Received DELETE request to eliminate a user");
         const response = await this.userService.deleteUser(username)
+        console.log(response);
         return res.send(response)
     }
 }
